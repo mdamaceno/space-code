7f7dd516c121517977032b007a3442a9ab72908d
Marco Damaceno <maadamaceno@gmail.com>
2023-10-27 12:32:02 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_26_225926) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_27_013117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pilots", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "certification", limit: 7, null: false
+    t.string "name", null: false
+    t.integer "age", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["certification"], name: "index_pilots_on_certification", unique: true
+  end
 
   create_table "planets", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
