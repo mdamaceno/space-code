7fb6c4fc166f3f5707d70c7c6b1843b6c6269d12
Marco Damaceno <maadamaceno@gmail.com>
2023-10-26 22:25:05 -0300

    seed planets and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+planets = [
+  Planet.find_or_create_by(name: "Andvari"),
+  Planet.find_or_create_by(name: "Demeter"),
+  Planet.find_or_create_by(name: "Aqua"),
+  Planet.find_or_create_by(name: "Calas"),
+]
+
+Route.find_or_create_by(origin_planet_id: planets[0].id, destination_planet_id: planets[1].id, blocked: true)
+Route.find_or_create_by(origin_planet_id: planets[0].id, destination_planet_id: planets[2].id, fuel_cost: 13)
+Route.find_or_create_by(origin_planet_id: planets[0].id, destination_planet_id: planets[3].id, fuel_cost: 23)
+
+Route.find_or_create_by(origin_planet_id: planets[1].id, destination_planet_id: planets[0].id, blocked: true)
+Route.find_or_create_by(origin_planet_id: planets[1].id, destination_planet_id: planets[2].id, fuel_cost: 22)
+Route.find_or_create_by(origin_planet_id: planets[1].id, destination_planet_id: planets[3].id, fuel_cost: 25)
+
+Route.find_or_create_by(origin_planet_id: planets[2].id, destination_planet_id: planets[0].id, blocked: true)
+Route.find_or_create_by(origin_planet_id: planets[2].id, destination_planet_id: planets[1].id, fuel_cost: 30)
+Route.find_or_create_by(origin_planet_id: planets[2].id, destination_planet_id: planets[3].id, fuel_cost: 12)
+
+Route.find_or_create_by(origin_planet_id: planets[3].id, destination_planet_id: planets[0].id, fuel_cost: 20)
+Route.find_or_create_by(origin_planet_id: planets[3].id, destination_planet_id: planets[1].id, fuel_cost: 25)
+Route.find_or_create_by(origin_planet_id: planets[3].id, destination_planet_id: planets[2].id, fuel_cost: 15)
