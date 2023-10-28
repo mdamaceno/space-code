fcaa6e795b4aaf2a90b628717f56aaa0c2c2cf98
Marco Damaceno <maadamaceno@gmail.com>
2023-10-28 19:14:22 -0300

    contract validation

---
+++ app/models/contract.rb
@@ -0,0 +1,11 @@
+class Contract < ApplicationRecord
+  belongs_to :ship, optional: true
+  belongs_to :planet, foreign_key: :origin_planet_id
+  belongs_to :planet, foreign_key: :destination_planet_id
+
+  validates :description, presence: true, length: { maximum: 255 }
+  validates :origin_planet_id, presence: true
+  validates :destination_planet_id, presence: true
+  validates :completed_at, comparison: { greater_than_or_equal_to: Time.zone.now }, allow_nil: true
+  validates :value, presence: true, numericality: { greater_than_or_equal_to: 0 }
+end
