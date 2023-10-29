21f96e66aec2526c16591a3ad53823063030cd2d
Marco Damaceno <maadamaceno@gmail.com>
2023-10-29 12:05:58 -0300

    refactor: use setup to define valid attributes

--- test/models/pilot_test.rb
+++ test/models/pilot_test.rb
@@ -1,61 +1,63 @@
 require "test_helper"
 
 class PilotTest < ActiveSupport::TestCase
+  setup do
+    @valid_attributes = {
+      name: 'Bo Katan',
+      age: 18,
+      certification: '7635290',
+      location: 'space',
+    }
+  end
+
   test "name is required" do
-    pilot = Pilot.new
-    assert_not pilot.valid?
+    pilot = Pilot.new(@valid_attributes.merge(name: nil))
     assert_attribute_contains_error pilot, :name, :blank
   end
 
   test "name should not be less than 3 characters" do
-    pilot = Pilot.new(name: "ab")
+    pilot = Pilot.new(@valid_attributes.merge(name: "ab"))
     assert_attribute_contains_error pilot, :name, :too_short
   end
 
   test "name should not be more than 150 characters" do
-    pilot = Pilot.new(name: "a" * 151)
+    pilot = Pilot.new(@valid_attributes.merge(name: "a" * 151))
     assert_attribute_contains_error pilot, :name, :too_long
   end
 
   test "age is required" do
-    pilot = Pilot.new
-    assert_not pilot.valid?
+    pilot = Pilot.new(@valid_attributes.merge(age: nil))
     assert_attribute_contains_error pilot, :age, :blank
   end
 
   test "age should not be less than 18" do
-    pilot = Pilot.new(age: 17)
-    assert_not pilot.valid?
+    pilot = Pilot.new(@valid_attributes.merge(age: 17))
     assert_attribute_contains_error pilot, :age, :greater_than_or_equal_to
   end
 
   test "certification is required" do
-    pilot = Pilot.new
-    assert_not pilot.valid?
+    pilot = Pilot.new(@valid_attributes.merge(certification: nil))
     assert_attribute_contains_error pilot, :certification, :blank
   end
 
   test "certification should contain 7 characters" do
-    pilot = Pilot.new(certification: "123456")
-    assert_not pilot.valid?
+    pilot = Pilot.new(@valid_attributes.merge(certification: "123456"))
     assert_attribute_contains_error pilot, :certification, :wrong_length
   end
 
   test "certification should be unique" do
-    pilot = Pilot.new(certification: pilots(:hans_solo).certification)
-    assert_not pilot.valid?
+    pilot = Pilot.new(@valid_attributes.merge(certification: pilots(:hans_solo).certification))
     assert_attribute_contains_error pilot, :certification, :taken
   end
 
   test "certification should be valid if check digit (last) is incorrect" do
-    pilot = Pilot.new(name: "Bo Katan", age: 28, certification: "1234567")
-    assert_not pilot.valid?
+    pilot = Pilot.new(@valid_attributes.merge(certification: "1234567"))
     assert_attribute_contains_error pilot, :certification, :invalid
   end
 
   test "certification should be valid if check digit (last) is correct" do
     ["987655-7", "632988-3", "774199-4"].each do |certification|
-      pilot = Pilot.new(name: "Bo Katan", age: 28, certification: certification)
+      pilot = Pilot.new(@valid_attributes.merge(certification: certification))
       assert pilot.valid?
     end
   end
