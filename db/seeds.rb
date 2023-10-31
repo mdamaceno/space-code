8c2f8200ca75e7367369f04f70430fd905956e50
Marco Damaceno <maadamaceno@gmail.com>
2023-10-31 00:11:03 -0300

    define default ids for planets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 planets = [
-  Planet.find_or_create_by(name: "Andvari"),
-  Planet.find_or_create_by(name: "Demeter"),
-  Planet.find_or_create_by(name: "Aqua"),
-  Planet.find_or_create_by(name: "Calas"),
+  Planet.find_or_create_by(name: "Andvari", id: "bd664ba6-7791-11ee-8601-a8a1599ad2d2"),
+  Planet.find_or_create_by(name: "Demeter", id: "c94ff5f2-7791-11ee-b4ad-a8a1599ad2d2"),
+  Planet.find_or_create_by(name: "Aqua", id: "d0107542-7791-11ee-862c-a8a1599ad2d2"),
+  Planet.find_or_create_by(name: "Calas", id: "d7430cc6-7791-11ee-a929-a8a1599ad2d2"),
 ]
 
 Route.find_or_create_by(origin_planet_id: planets[0].id, destination_planet_id: planets[1].id, blocked: true)
